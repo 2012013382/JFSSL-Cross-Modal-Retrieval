d3377b0072c6d8fc6476dd21da61ed504f1b3a9e
StOnEGiggity <tangtianqi@outlook.com>
2017-04-15 21:55:54 +0800

    could display but exist some problem

--- README.txt
+++ README.txt
@@ -4,3 +4,5 @@ Email: Changsu_Liao@163.com
 Development time: 2017-3-23 -- 2017-3-24
 Data: wikipedia_info(2173 train data, 693 test data)
 Usage method: Run JFSSL.m
+
+please put wikipedia_dataset on the root folder
--- get_MAP_result.m
+++ get_MAP_result.m
@@ -24,8 +24,37 @@ image_AP = zeros(DATA_NUM, 1);
 image_AP_count = zeros(DATA_NUM, 1);
 image_AP_count(1:DATA_NUM, 1) = 1;
 fprintf('Calculating MAP of image query...\n');
+ClassInd = textread('wikipedia_info/trainset_txt_img_cat.list','%s','delimiter','\n');
+baseDir = './wikipedia_dataset';
 
 for i = 1:DATA_NUM
+    %new added code
+    temp = regexp(ClassInd(i),'\t','split');
+    image = [temp{1}{2},'.jpg'];
+    text = [temp{1}{1},'.xml'];
+    fprintf(['This is our image input: ',image,'\n']);
+    imageDir=[baseDir,'/images'];
+    subdir=dir(imageDir);
+    for x = 1:length(subdir)
+        if(isequal(subdir(x).name,'.')||...
+                isequal(subdir(x).name,'..')||...
+                ~subdir(x).isdir)
+            continue;
+        end
+        subdirpath=fullfile(imageDir,subdir(x).name,image);
+        dat=dir(subdirpath);
+        for y=1:length(dat)
+            if(~dat(y).isdir)
+                imshow(subdirpath);
+            end
+        end
+    end
+    pause();
+    fprintf(['This is the corresponding text: ',text,'\n']);
+    textDir=fullfile(baseDir,'texts');
+    temp_t=xml2struct(fullfile(textDir,text));
+    disp(temp_t.document.text.Text);
+    pause();
     for j = 1:DATA_NUM
         if train_or_test == 0
             if data.train(i, 1) == data.train(image_similarity_index_M(i, j), 1)
@@ -36,6 +65,14 @@ for i = 1:DATA_NUM
             if data.test(i, 1) == data.test(image_similarity_index_M(i, j), 1)
                 image_AP(i, 1) = image_AP(i, 1) + image_AP_count(i, 1) / j;
                 image_AP_count(i, 1) = image_AP_count(i, 1) + 1;
+                num = image_similarity_index_M(i,j);
+                temp2 = regexp(ClassInd(num),'\t','split');
+                text2 = [temp2{1}{1},'.xml'];
+                fprintf(['This is we find text: ',text2,'\n']);
+                textDir=fullfile(baseDir,'texts');
+                temp_t=xml2struct(fullfile(textDir,text2));
+                disp(temp_t.document.text.Text);
+                pause();
             end
         end
     end
@@ -49,6 +86,32 @@ text_AP_count = zeros(DATA_NUM, 1);
 text_AP_count(1:DATA_NUM, 1) = 1;
 fprintf('Calculating MAP of text query...\n');
 for j = 1:DATA_NUM
+    temp = regexp(ClassInd(j),'\t','split');
+    image = [temp{1}{2},'.jpg'];
+    text = [temp{1}{1},'.xml'];
+    fprintf(['This is our corresponding image: ',image,'\n']);
+    imageDir=[baseDir,'/images'];
+    subdir=dir(imageDir);
+    for x = 1:length(subdir)
+        if(isequal(subdir(x).name,'.')||...
+                isequal(subdir(x).name,'..')||...
+                ~subdir(x).isdir)
+            continue;
+        end
+        subdirpath=fullfile(imageDir,subdir(x).name,image);
+        dat=dir(subdirpath);
+        for y=1:length(dat)
+            if(~dat(y).isdir)
+                imshow(subdirpath);
+            end
+        end
+    end
+    pause();
+    fprintf(['This is our input text: ',text,'\n']);
+    textDir=fullfile(baseDir,'texts');
+    temp_t=xml2struct(fullfile(textDir,text));
+    disp(temp_t.document.text.Text);
+    pause();
     for i = 1:DATA_NUM
         if train_or_test == 0
             if data.train(j, 1) == data.train(text_similarity_index_M(i, j), 1)
@@ -59,6 +122,27 @@ for j = 1:DATA_NUM
             if data.test(j, 1) == data.test(text_similarity_index_M(i, j), 1)
                 text_AP(j, 1) = text_AP(j, 1) + text_AP_count(j, 1) / i;
                 text_AP_count(j, 1) = text_AP_count(j, 1) + 1;
+                num = image_similarity_index_M(i,j);
+                temp2 = regexp(ClassInd(num),'\t','split');
+                image2 = [temp2{1}{2},'.jpg'];
+                fprintf(['This is we find image: ',image2,'\n']);
+                imageDir=[baseDir,'/images'];
+                subdir=dir(imageDir);
+                for x = 1:length(subdir)
+                    if(isequal(subdir(x).name,'.')||...
+                            isequal(subdir(x).name,'..')||...
+                            ~subdir(x).isdir)
+                        continue;
+                    end
+                    subdirpath=fullfile(imageDir,subdir(x).name,image);
+                    dat=dir(subdirpath);
+                    for y=1:length(dat)
+                        if(~dat(y).isdir)
+                            imshow(subdirpath);
+                        end
+                    end
+                end
+                pause();
             end
         end
     end
